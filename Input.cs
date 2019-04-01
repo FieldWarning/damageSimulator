cdef57317f33a5f4ebab07597a6c3edc43fe04ee
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2019-04-01 13:12:58 +0200

    Add files via upload
    New BETA, KE/HEAT works, but ERA needs some tuning

---
+++ Input.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using PhysicalDamage.Core;
+using static PhysicalDamage.Core.PhysicalProjectile;
+
+namespace PhysicalDamage
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var era = new ERA { Bonus = 2.0f, ExplosiveFactor = 1.0f, PiercingFactor = 1.0f, Plates = 1 };
+            var heat = new Heat { Piercing = 0f, ArmorHeatFactor = 0.5f, HealthHeatFactor = 1f };
+            var kinetic = new Kinetic { Piercing = 20f, ArmorKineticFactor = 1f, HealthKineticFactor = 0.5f };
+            var vehicle = new Vehicle { Armor = 20f, Health = 10f };
+
+
+            var dmg = PhysicalProjectile.GetTargetDamage(kinetic, heat, vehicle, era);
+            var armor = dmg[0];
+            var health = dmg[1];
+            var plates = dmg[2];
+
+
+            /* long startingTime = Stopwatch.GetTimestamp();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                
+            }
+
+            long endingTime = Stopwatch.GetTimestamp();
+            long elapsedTime = endingTime - startingTime; 
+
+            double elapsedSeconds = elapsedTime * (1.0 / Stopwatch.Frequency); 
+            
+             Console.WriteLine($"5000 Cycles -> {elapsedSeconds}ms"); 
+            */
+
+            Console.WriteLine($"Armor: {armor} - HP: {health} - ERA Plates: {plates}");
+
+           
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
--- PhysicalProjectile.cs
+++ PhysicalProjectile.cs
@@ -1,73 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
-namespace Damage_Simulation_Algo.DamageClasses
+namespace PhysicalDamage.Core
 {
-    static class PhysicalProjectile
+    public static class PhysicalProjectile
     {
         public struct Kinetic
         {
             public float Piercing;
-            public float ArmorKineticMultiplier;
-            public float HealthKineticMultiplier;
+
+            public float ArmorKineticFactor;
+
+            public float HealthKineticFactor;
         }
 
         public struct Heat
         {
             public float Piercing;
-            public float ArmorHeatMultiplier;
-            public float HealthHeatMultiplier;
+
+            public float ArmorHeatFactor;
+
+            public float HealthHeatFactor;
         }
 
         public struct Vehicle
         {
             public float Armor;
+
             public float Health;
         }
 
         public struct ERA
         {
             public int Plates;
+
             public float Bonus;
-            public float PiercingMultiplier;
-            public float ExplosiveMultiplier;
+
+            public float PiercingFactor;
+
+            public float ExplosiveFactor;
         }
 
         /// <summary>
-        /// Returns how much armor and health is left, possibly if the target has died.
-        /// Returns are the following: 0 is Remaining AV, 1 is Remaining HP
+        /// Calculates the amount of damage for *blank*
         /// </summary>
+        /// <param name="kinetic">Kinetic variables of equation.</param>
+        /// <param name="heat">Heat variables.</param>
+        /// <param name="vehicle">Vehicle variables.</param>
+        /// <param name="era">ERA variables.</param>
+        /// <returns><see cref="System.Single">float[3]</see></returns>
         public static float[] GetTargetDamage(Kinetic kinetic, Heat heat, Vehicle vehicle, ERA era)
         {
             float armorPiercingDamage = 0f;
             float heatPiercingDamage = 0f;
-            bool ERABlocksShot = false;
+            //bool ERABlocksShot = false;
 
             // Kinetic Armor Degradation
             if (era.Plates < 1 && vehicle.Armor > 0)
             {
-                armorPiercingDamage = (kinetic.Piercing / vehicle.Armor) * kinetic.ArmorKineticMultiplier;
+                armorPiercingDamage = (kinetic.Piercing / vehicle.Armor) * kinetic.ArmorKineticFactor;
                 Debug.WriteLine($"Armor Piercing Damage: {armorPiercingDamage}");
+                Console.WriteLine($"Armor Piercing Damage: {armorPiercingDamage}");
             }
 
             if (era.Plates > 0)
             {
-                float eraArmor = era.Bonus;
-                armorPiercingDamage = (kinetic.Piercing / era.Bonus) * kinetic.ArmorKineticMultiplier;
-                eraArmor = era.Bonus - (armorPiercingDamage * era.PiercingMultiplier);
+                armorPiercingDamage = (kinetic.Piercing / era.Bonus) * era.PiercingFactor;
+                float eraArmor = era.Bonus - (armorPiercingDamage * era.PiercingFactor);
+
+
+
 
                 if (eraArmor > 0)
-                {
-                    armorPiercingDamage = 0;
-                }
-                else if (vehicle.Armor > 0)
-                {
-                    armorPiercingDamage = ((eraArmor * -1) / vehicle.Armor) * kinetic.ArmorKineticMultiplier;
-                }
+                    armorPiercingDamage -= eraArmor;
+                if (eraArmor < 0)
+                    eraArmor = 0;
+
+                if (vehicle.Armor > 0)
+                    armorPiercingDamage = ( kinetic.Piercing / vehicle.Armor) * kinetic.ArmorKineticFactor;
 
             }
 
@@ -75,87 +86,90 @@ namespace Damage_Simulation_Algo.DamageClasses
 
             // Heat Armor Degradation
 
-            /*heatPiercingDamage = (era.Plates < 1 && vehicle.Armor > 0) ? 
-                                     (heat.Piercing / vehicle.Armor) * heat.ArmorHeatMultiplier :
-                                      heatPiercingDamage;*/
-
             if (era.Plates < 1 && vehicle.Armor > 0)
             {
-                heatPiercingDamage = (heat.Piercing / vehicle.Armor) * heat.ArmorHeatMultiplier;
-                Debug.WriteLine($"Heat Piercing Damage: {heatPiercingDamage}");
+                heatPiercingDamage = (heat.Piercing / vehicle.Armor) * heat.ArmorHeatFactor;
+                
             }
 
             if (era.Plates > 0)
             {
-                float eraArmor = era.Bonus;
-                heatPiercingDamage = (heat.Piercing / era.Bonus) * heat.HealthHeatMultiplier;
-                eraArmor = era.Bonus - (heatPiercingDamage * era.ExplosiveMultiplier);
+                heatPiercingDamage = (heat.Piercing / era.Bonus) * heat.HealthHeatFactor;
+                float eraArmor = era.Bonus - (heatPiercingDamage * era.ExplosiveFactor);
 
-                if (era.Bonus > 0)
-                {
-                    heatPiercingDamage = 0;
-                }
+
+
+                if (eraArmor > 0)
+                    heatPiercingDamage -= eraArmor;
+                if (eraArmor < 0)
+                    eraArmor = 0;
                 else if (vehicle.Armor > 0)
-                {
-                    heatPiercingDamage = ((eraArmor * -1) / vehicle.Armor) * heat.ArmorHeatMultiplier;
-                } 
+                    heatPiercingDamage = (heat.Piercing / vehicle.Armor) * heat.ArmorHeatFactor;
             }
 
             Debug.WriteLine($"Heat Piercing Damage: {heatPiercingDamage}");
+            Console.WriteLine($"Heat Piercing Damage: {heatPiercingDamage}");
 
-            
-            
+            if (armorPiercingDamage < 0) armorPiercingDamage = 0;
+            if (heatPiercingDamage < 0) heatPiercingDamage = 0;
+
             // Total armor degradation
             var totalDegradation = armorPiercingDamage + heatPiercingDamage;
             vehicle.Armor -= totalDegradation;
             if (vehicle.Armor < 0) vehicle.Armor = 0;
             Debug.WriteLine($"Total Degradation: {totalDegradation}");
             Debug.WriteLine($"Vehicle Armor: {vehicle.Armor}");
-            
+            Console.WriteLine($"Total Degradation: {totalDegradation}");
+            Console.WriteLine($"Vehicle Armor: {vehicle.Armor}");
+
+            // HP damage
+
             // AP -> HP damage
-            float kineticHealthDamage = (kinetic.Piercing - vehicle.Armor) * kinetic.HealthKineticMultiplier;
-            if (kineticHealthDamage < 0) kineticHealthDamage = 0;
+            float kineticHealthDamage = (kinetic.Piercing - vehicle.Armor) * kinetic.HealthKineticFactor;
             
 
+
             Debug.WriteLine($"Kinetic Health Damage: {kineticHealthDamage}");
-            
+            Console.WriteLine($"Kinetic Health Damage: {kineticHealthDamage}");
+
             // HE -> HP damage
-            float heatHealthDamage = (heat.Piercing - vehicle.Armor) * heat.HealthHeatMultiplier;
-            if (heatHealthDamage < 0) kineticHealthDamage = 0;
+            float heatHealthDamage = (heat.Piercing - vehicle.Armor) * heat.HealthHeatFactor;
+            
 
 
             if (vehicle.Armor == 0 && era.Plates > 0)
             {
                 kineticHealthDamage -= era.Bonus;
                 heatHealthDamage -= era.Bonus;
-            } 
+            }
+
             era.Plates = era.Plates - 1;
 
             Debug.WriteLine($"ERA Plates: {era.Plates}");
+            Console.WriteLine($"ERA Plates: {era.Plates}");
 
             // Total health reduction
+            if (kineticHealthDamage < 0) kineticHealthDamage = 0;
+            if (heatHealthDamage < 0) heatHealthDamage = 0;
             vehicle.Health = vehicle.Health - (kineticHealthDamage + heatHealthDamage);
 
             // just to prevent any bugs, i'm resetting any negative values
 
             if (era.Plates < 0)
-            {
                 era.Plates = 0;
-            }
+
             if (vehicle.Armor < 0)
-            {
                 vehicle.Armor = 0;
-            }
+
             if (vehicle.Health < 0)
-            {
                 vehicle.Health = 0;
-            }
 
             Math.Round(vehicle.Armor, 2);
             Math.Round(vehicle.Health, 2);
 
-            return new float[] {vehicle.Armor, vehicle.Health, era.Plates};
+            Console.WriteLine($"After shot: {vehicle.Armor} AV, {vehicle.Health} HP, {era.Plates} ERA Plates");
+
+            return new float[] { vehicle.Armor, vehicle.Health, era.Plates };
         }
     }
 }
-
