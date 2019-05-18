22d306093bebcba8671071f046f0c33f4fdcc4ff
Yang Xiao <1125099738@qq.com>
2019-05-18 18:05:52 +0800

    Fire and HE WIP

---
+++ FireDamage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PhysicalDamage.Core
+{
+    class FireDamage : Damage
+    {
+        public struct FireData
+        {
+            public float Power;
+        }
+
+        private FireData _fireData;
+
+        public FireDamage(FireData data, Target target) : base(DamageTypes.FIRE, target)
+        {
+            _fireData = data;
+        }
+
+        public override Target CalculateDamage()
+        {
+            Target finalState = this.CurrentTarget;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HEDamage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PhysicalDamage.Core
+{
+    class HEDamage : Damage
+    {
+        public struct HEData
+        {
+            /// <summary>
+            /// The power of the shot
+            /// </summary>
+            public float Power;
+            /// <summary>
+            /// The radius of the explosion
+            /// Beyond the effective radius, the value {Remaining damage}/{Initial damage} is less than CUTOF_FFRACTION
+            /// </summary>
+            public float EffectiveRadius;
+        }
+
+        private HEData _heData;
+        private float _distanceToCentre;
+
+        /// <summary>
+        /// The cutoff fraction for HE damage splash
+        /// Read documentation {4.2 HE dropoff within AOE} for details
+        /// </summary>
+        private const float CUTOFF_FRACTION = 0.01f; // Relocate this constant elsewhere when integrating
+
+        public HEDamage(HEData data, Target target, float distanceToCentre) : base(DamageTypes.HE, target)
+        {
+            _heData = data;
+            _distanceToCentre = distanceToCentre;
+        }
+
+        public override Target CalculateDamage()
+        {
+            Target finalState = this.CurrentTarget;
+            throw new NotImplementedException();
+        }
+
+        private float CalculatedPowerDropoff(float power, float radius, float distance)
+        {
+            if (distance > radius)
+            {
+                return 0.0f;
+            }
+            else
+            {
+                float finalPower = (float)(power / (4 * Math.PI * distance * distance));
+                return finalPower;
+            }
+        }
+    }
+}
